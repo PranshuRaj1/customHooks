ca916405af09571f0b1f61223b3dc3332fa1d582
Pranshu Raj <rajpranshu2222@gmail.com>
2024-09-13 19:17:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const todo = UseTodos("");
+  return (
+    <>
+      {/* {todos.map((todos) => (
+        <Track todo={todos} />
+      ))} */}
+    </>
+  );
+}
+
+//custom hooks
+
+export default App;
---
+++ src/Hooks/UseTodo.jsx
@@ -0,0 +1,24 @@
+export default function UseTodos(url, loading, timeInSeconds) {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const valueOfInterval = setInterval(() => {
+      axios.get(url).then((res) => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      });
+    }, timeInSeconds * 1000);
+
+    axios.get(url).then((res) => {
+      setTodos(res.data.todos);
+      setLoading(false);
+    });
+
+    return () => {
+      clearInterval(valueOfInterval);
+    };
+  }, [timeInSeconds]);
+
+  return { todos, loading };
+}
